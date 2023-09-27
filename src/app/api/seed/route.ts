e56600df876865de7c59298e6b242e0f744ff528
AndresSalomonMidas <asalomon@midasconsultores.com.ar>
2023-09-27 17:18:43 -0300

    Create SEED for initialize the local BD

---
+++ src/app/api/seed/route.ts
@@ -0,0 +1,22 @@
+import prisma from '@/lib/prisma';
+import { NextResponse } from 'next/server';
+
+export async function GET() {
+  // Inserción en la BD
+  // La idea del Seed es que purgue la BD antes de insertar otro dato
+  await prisma.todo.deleteMany(); // DELETE * FROM todo
+
+  const todo = await prisma.todo.createMany({
+    data: [
+      { description: 'Piedra del alma', complete: true },
+      { description: 'Piedra del poder' },
+      { description: 'Piedra del tiempo' },
+      { description: 'Piedra del espacio' },
+      { description: 'Piedra de la realidad' },
+    ],
+  });
+
+  console.log(todo);
+
+  return NextResponse.json({ message: 'Seed Executed' });
+}
