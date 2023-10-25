51de3736e87b7dc78666bf4f45a453cdd6fc25b2
AndresSalomonMidas <asalomon@midasconsultores.com.ar>
2023-10-24 23:57:29 -0300

    Sistema de navegación

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,7 +1,26 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import { CiLogout } from 'react-icons/ci';
+import { IoCalendarOutline, IoCheckboxOutline, IoListOutline } from 'react-icons/io5';
 import { SidebarItem } from '../SidebarItem/SidebarItem';
+
+const menuItems = [
+  {
+    icon: <IoCalendarOutline />,
+    title: 'Dashboard',
+    path: '/dashboard',
+  },
+  {
+    icon: <IoCheckboxOutline />,
+    title: 'Rest TODOS',
+    path: '/dashboard/rest-todos',
+  },
+  {
+    icon: <IoListOutline />,
+    title: 'Server Actions',
+    path: '/dashboard/server-todos',
+  },
+];
 
 export const Sidebar = () => (
   <aside className="ml-[-100%] fixed z-10 top-0 pb-3 px-6 w-full flex flex-col justify-between h-screen border-r bg-white transition duration-300 md:w-4/12 lg:ml-0 lg:w-[25%] xl:w-[20%] 2xl:w-[15%]">
@@ -34,9 +53,9 @@ export const Sidebar = () => (
       </div>
 
       <ul className="space-y-2 tracking-wide mt-8">
-        {/* TODO: src/components <SidebarItem /> */}
-        {/* Active className: text-white bg-gradient-to-r from-sky-600 to-cyan-400 */}
-        <SidebarItem />
+        {menuItems.map((item) => (
+          <SidebarItem key={item.path} {...item} />
+        ))}
       </ul>
     </div>
 
