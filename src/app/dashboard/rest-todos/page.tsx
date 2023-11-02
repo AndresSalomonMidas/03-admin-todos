7fb003d83652d79e0cbb0676fdb1b0790fb34b15
AndresSalomonMidas <asalomon@midasconsultores.com.ar>
2023-11-01 23:15:47 -0300

    List todos

--- src/app/dashboard/rest-todos/page.tsx
+++ src/app/dashboard/rest-todos/page.tsx
@@ -1,7 +1,34 @@
-export default function RestTodosPage() {
+import prisma from '@/lib/prisma';
+
+export const metadata = {
+  title: 'Listado de todos',
+  description: 'Listado de todos',
+};
+
+/*
+  Para usar una url en el fetch:
+  Client component: url relativa
+  Server component: url absoluta
+*/
+
+export default async function RestTodosPage() {
+  // Client
+  // useEffect(() => {
+  //   fetch('/api/todos')
+  //     .then((resp) => resp.json())
+  //     .then((json) => console.log(json));
+  // }, []);
+
+  const todos = await prisma.todo.findMany({ orderBy: { description: 'asc' } });
+
   return (
     <div>
       <h1>RestTodos Page</h1>
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id}>{todo.description}</li>
+        ))}
+      </ul>
     </div>
   );
 }
