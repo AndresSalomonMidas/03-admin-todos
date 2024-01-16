70e24872bd70b0f8bb258f2161de9f123b7af52b
AndresSalomonMidas <asalomon@midasconsultores.com.ar>
2024-01-15 23:58:04 -0300

    Setup Github provider

--- src/components/WidgetItem/WidgetItem.tsx
+++ src/components/WidgetItem/WidgetItem.tsx
@@ -10,7 +10,7 @@ export const WidgetItem = ({ title, children }: Props) => (
     <div className="h-full py-8 px-6 space-y-6 rounded-xl border border-gray-200 bg-white">
       <div className='flex flex-col'>
         <h5 className="text-xl text-gray-600 text-center">{title}</h5>
-        <div className="mt-2 flex flex-col justify-center gap-4">
+        <div className="mt-2 flex flex-col justify-center gap-4 text-black">
           {children}
         </div>
       </div>
