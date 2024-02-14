3f823b566ce859df54531e7f743f805175e7c405
AndresSalomonMidas <asalomon@midasconsultores.com.ar>
2024-02-13 21:15:22 -0300

    Add Credentials Provider

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -2,9 +2,11 @@
 import NextAuth, { type NextAuthOptions } from 'next-auth';
 import GithubProvider from 'next-auth/providers/github';
 import GoogleProvider from 'next-auth/providers/google';
+import CredentialsProvider from 'next-auth/providers/credentials';
 import { PrismaAdapter } from '@auth/prisma-adapter';
 import prisma from '@/lib/prisma';
 import { Adapter } from 'next-auth/adapters';
+import { signInEmailPassword } from '@/app/auth/actions/auth-actions';
 
 export const authOptions: NextAuthOptions = {
   adapter: PrismaAdapter(prisma) as Adapter,
@@ -16,6 +18,34 @@ export const authOptions: NextAuthOptions = {
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID ?? '',
       clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? '',
+    }),
+    CredentialsProvider({
+      // The name to display on the sign in form (e.g. "Sign in with...")
+      name: 'Credentials',
+      // `credentials` is used to generate a form on the sign in page.
+      // You can specify which fields should be submitted,
+      // by adding keys to the `credentials` object.
+      // e.g. domain, username, password, 2FA token, etc.
+      // You can pass any HTML attribute to the <input> tag through the object.
+      credentials: {
+        email: { label: 'Email', type: 'email', placeholder: 'usuario@email.com' },
+        password: { label: 'Contraseña', type: 'password', placeholder: '********' },
+      },
+      async authorize(credentials) {
+        // Add logic here to look up the user from the credentials supplied
+        const user = await signInEmailPassword(credentials?.email ?? '', credentials?.password ?? '');
+
+        if (user) {
+        // Any object returned will be saved in `user` property of the JWT
+          return user;
+        }
+        // If you return null then an error will be displayed
+        // advising the user to check their details.
+        return null;
+
+        // You can also Reject this callback with an Error
+        // thus the user will be sent to the error page with the error message as a query parameter
+      },
     }),
   ],
   session: {
